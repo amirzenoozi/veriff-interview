b0afc3e34064a566bd9316c4cf82ed58a0a65c61
amirzenoozi <amirzenoozi72@gmail.com>
2024-07-01 20:55:24 +0200

    :tada: Chore: Set up the Backend Codes

---
+++ back/README.md
@@ -0,0 +1,37 @@
+## Description
+
+This project was bootstrapped with [Nest](https://github.com/nestjs/nest).
+his project lets you create the survey questions and store the submitted answers.
+
+## Objectives
+
+- [x] Store Survey Questions
+- [x] Store Survey Answers
+- [ ] Unit Test
+    - [x] Validate Answers
+- [ ] Deployment
+    - [x] Dockerfile
+    - [x] GitHub Actions to Push Image to DockerHub
+    - [x] Deploy on external server
+
+## Run the project
+
+In the project directory, you can run:
+- ``` git clone git@github.com:amirzenoozi/veriff-interview.git ```
+- ``` git config core.hooksPath .githooks ```
+- ``` cd veriff-interview/back ```
+- ``` yarn install ```
+- ``` yarn start ```
+- Open [http://localhost:3200](http://localhost:3000) to view it in the browser.
+
+## Available Scripts
+
+1. For Development: `yarn run start`
+2. For Watch Mode: `yarn run start:dev`
+3. For Production Mode: `yarn run start:prod`
+
+## Test Scripts
+
+1. Unit Tests: `yarn run test`
+2. E2E Test: `yarn run test:e2e`
+3. Test Coverage: `yarn run test:cov`
---
+++ back/src/app.module.ts
@@ -0,0 +1,38 @@
+import { Module, ValidationPipe } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ResponseService } from './response/response.service';
+import { VerificationSchema } from './verification/verification.schema';
+import { ResponseSchema } from './response/response.schema';
+import { VerificationController } from './verification/verification.controller';
+import { VerificationService } from './verification/verification.service';
+import { ResponseController } from './response/response.controller';
+import { APP_PIPE } from '@nestjs/core';
+import * as dotenv from 'dotenv';
+import * as process from 'node:process';
+
+dotenv.config();
+
+@Module({
+  imports: [
+    MongooseModule.forRoot(process.env.MONGODB_URI),
+    MongooseModule.forFeature([
+      { name: 'Verification', schema: VerificationSchema },
+      { name: 'Response', schema: ResponseSchema },
+    ]),
+  ],
+  controllers: [AppController, VerificationController, ResponseController],
+  providers: [
+    AppService,
+    VerificationService,
+    ResponseService,
+    {
+      provide: APP_PIPE,
+      useValue: new ValidationPipe({
+        whitelist: true,
+      }),
+    },
+  ],
+})
+export class AppModule {}
