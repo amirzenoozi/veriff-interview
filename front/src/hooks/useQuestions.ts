0ac6589f2b6ca821e43f55eec7b617e72908a2b4
amirzenoozi <amirzenoozi72@gmail.com>
2024-07-03 10:35:31 +0200

    :zap: Feat: Change The Hooks in The Way That Works With Params

--- front/src/hooks/useQuestions.ts
+++ front/src/hooks/useQuestions.ts
@@ -4,16 +4,13 @@ import { getVerifications } from '../modules/api'
 const useQuestions = (limit: number, page: number, orderBy: string, order: string) => {
 	const [items, setItems] = useState<any[]>([])
 	const [fetching, setFetching] = useState<boolean>(true)
-	const [verificationId, setVerificationId] = useState<string>('')
 	const [error, setError] = useState<any>(undefined)
 
 	// Get questions from API
 	useEffect(() => {
 		setFetching(true)
 		getVerifications(limit, page, orderBy, order).then((res: any) => {
-			const sortedQuestions = res.data[0].questions.sort((a: any, b: any) => a.priority - b.priority)
-			setVerificationId(res.data[0].uuid)
-			setItems(sortedQuestions)
+			setItems(res.data)
 		}).catch((e) => {
 			setError(e)
 		}).finally(() => {
@@ -21,7 +18,7 @@ const useQuestions = (limit: number, page: number, orderBy: string, order: strin
 		})
 	}, [])
 
-	return { questions: items, fetching, verificationId, error }
+	return { questions: items, fetching, error }
 }
 
 export default useQuestions
