182ccb1812df20321bdb1cbb8a81586a466caf5f
amirzenoozi <amirzenoozi72@gmail.com>
2024-07-03 10:35:59 +0200

    :tada: Feat: Add Questions List View Component

---
+++ front/src/pages/questions-list/index.tsx
@@ -0,0 +1,79 @@
+import React, { useState } from 'react'
+import { type Check } from '../../interfaces/check'
+import Container from '../../components/container'
+import LoadingSpinner from '../../components/loading-spinner'
+import useQuestions from '../../hooks/useQuestions'
+import { ListCheckbox, Error } from '@icon-park/react'
+import { NavLink } from 'react-router-dom'
+import './style.scss'
+
+function QuestionsList () {
+	const [limit, setLimit] = useState(10)
+	const [pageNumber, setPageNumber] = useState(1)
+	const [orderBy, setOrderBy] = useState('created_at')
+	const [order, setOrder] = useState('desc')
+	const { questions, fetching, error } = useQuestions(limit, pageNumber, orderBy, order)
+	const [results, setResults] = useState<Check[]>([])
+
+	const checkDisabled = (index: number) => {
+		if (index === 0) {
+			return false
+		} else {
+			const answerIndex = results.findIndex((item) => item.checkId === questions[index - 1].id)
+			return answerIndex === -1 || results[answerIndex].result === 'no'
+		}
+	}
+
+	return (
+		<>
+			<section className={'page'}>
+				<Container size={'xs'}>
+					<div className={'form'}>
+						<div className={'form__content'}>
+							<ul className={'form__list'}>
+								{
+									questions?.map((item: any, index: number) => (
+										(
+											<li key={item.uuid}>
+												<NavLink to={`/questions/${item.uuid}`}>
+													<p>{item.uuid}</p>
+													<span>{ item.questions?.length }</span>
+												</NavLink>
+											</li>
+										)
+									))
+								}
+							</ul>
+						</div>
+						{
+							fetching && (
+								<div className={'form__overlay'}>
+									<LoadingSpinner size={'md'}/>
+									<p>We Are Fetching The Questions!</p>
+								</div>
+							)
+						}
+						{
+							!fetching && !error && questions.length === 0 && (
+								<div className={'form__overlay'}>
+									<ListCheckbox theme="outline" size="32" />
+									<p>There is no Available Question!</p>
+								</div>
+							)
+						}
+						{
+							!fetching && error && (
+								<div className={'form__overlay'}>
+									<Error theme="outline" size="32" />
+									<p>Internal Server Error!</p>
+								</div>
+							)
+						}
+					</div>
+				</Container>
+			</section>
+		</>
+	)
+}
+
+export default QuestionsList
