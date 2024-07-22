f315acabd05ea1114a4daf81d79711604803ae84
amirzenoozi <amirzenoozi72@gmail.com>
2024-07-22 16:59:31 +0200

    :sparkles: Feat: Add GraphQL Module to App

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -8,12 +8,22 @@ import { ResponseSchema } from './response/response.schema';
 import { VerificationController } from './verification/verification.controller';
 import { VerificationService } from './verification/verification.service';
 import { ResponseController } from './response/response.controller';
+import { VerificationResolver } from './verification/verification.resolver';
+import { ResponseResolver } from './response/response.resolver';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { APP_PIPE } from '@nestjs/core';
 import configuration from '../config/configuration';
+import { join } from 'path';
 
 @Module({
 	imports: [
 		MongooseModule.forRoot(configuration().database.mongo.uri),
+		GraphQLModule.forRoot<ApolloDriverConfig>({
+			driver: ApolloDriver,
+			playground: true,
+			autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+		}),
 		MongooseModule.forFeature([
 			{name: 'Verification', schema: VerificationSchema},
 			{name: 'Response', schema: ResponseSchema},
@@ -24,6 +34,8 @@ import configuration from '../config/configuration';
 		AppService,
 		VerificationService,
 		ResponseService,
+		VerificationResolver,
+		ResponseResolver,
 		{
 			provide: APP_PIPE,
 			useValue: new ValidationPipe({
