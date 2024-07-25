480e4a46c84a403055bd21866c96510e32f22fb1
amirzenoozi <amirzenoozi72@gmail.com>
2024-07-25 17:44:41 +0200

    :zap: Feat: Change GraphQl Schema Path

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -22,7 +22,7 @@ import { join } from 'path';
 		GraphQLModule.forRoot<ApolloDriverConfig>({
 			driver: ApolloDriver,
 			playground: true,
-			autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+			autoSchemaFile: 'schema.gql',
 		}),
 		MongooseModule.forFeature([
 			{name: 'Verification', schema: VerificationSchema},
